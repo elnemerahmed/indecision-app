d1eb531dfaab639a81346651dfe16bbb8c60d2d5
Ahmed El Nemer <aelnemer1@smail.ucas.edu.ps>
2020-08-05 00:11:54 +0300

    Edit App.js

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -62,6 +62,7 @@ export default class IndecisionApp extends React.Component {
   render() {
     const subtitle = 'Put your life in the hands of a computer';
 
+    
     return (
       <div>
         <Header subtitle={subtitle} />
